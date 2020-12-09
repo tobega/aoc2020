9eb644abaa6526ead0302967af5eeffef1c21485
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2020-12-09 20:35:57 +0100

    day 7 julia

---
+++ a7.jl
@@ -0,0 +1,26 @@
+rules = open("a7.txt") do file
+  Dict([match(r"(\w+ \w+) bags contain", line).captures[1]=>
+    Dict([m.captures[2]=>parse(Int, m.captures[1])
+      for m in eachmatch(r"(\d+) (\w+ \w+)", line)])
+        for line in eachline(file)])
+end
+
+function addcontainersof!(containers, bag)
+  for (colour, contents) in rules
+    if !in(containers, colour) && haskey(contents, bag)
+      push!(containers, colour)
+      addcontainersof!(containers, colour)
+    end
+  end
+  containers
+end
+
+part1 = length(addcontainersof!(Set(), "shiny gold"))
+println("$part1")
+
+function contains(bag, multiplier)::Int
+  multiplier * (1 + sum([contains(colour, count) for (colour, count) in rules[bag]]))
+end
+
+part2 = contains("shiny gold", 1) - 1
+println("$part2")
